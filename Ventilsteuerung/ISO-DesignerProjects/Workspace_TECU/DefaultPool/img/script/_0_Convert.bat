63d2797573c5d62038dc85c19150d44505a2ce2a
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2024-07-13 13:08:52 +0200

    Update image processing script to exclude metadata in PNG files
    The script now excludes date metadata from PNG files during cropping, resizing, and monochrome conversion for improved file handling.

--- Ventilsteuerung/ISO-DesignerProjects/Workspace_TECU/DefaultPool/img/script/_0_Convert.bat
+++ Ventilsteuerung/ISO-DesignerProjects/Workspace_TECU/DefaultPool/img/script/_0_Convert.bat
@@ -11,13 +11,13 @@ cd ..\img_original
 for %%f in (*.png) do (
     magick identify %%f
     echo 1. crop Bitmap by 2px 
-    magick %%f -shave   2x2 ..\img_cropped\%%f
+    magick %%f -shave   2x2 ..\img_cropped\%%f -define png:exclude-chunks=date
     magick identify ..\img_cropped\%%f
     echo 2. resize to 64x64
-    magick ..\img_cropped\%%f  -resize  64x64       ..\img_resized\%%f
+    magick ..\img_cropped\%%f  -resize  64x64       ..\img_resized\%%f  -define png:exclude-chunks=date
     magick identify ..\img_resized\%%f
     echo 3. make monochrome PNG
-    magick ..\img_cropped\%%f  -monochrome       ..\img_monochrome\%%f
+    magick ..\img_cropped\%%f  -monochrome       ..\img_monochrome\%%f -define png:exclude-chunks=date
     magick identify ..\img_monochrome\%%f
     echo 4. make BMP
     magick ..\img_monochrome\%%f    ..\img\%%~nf.bmp
