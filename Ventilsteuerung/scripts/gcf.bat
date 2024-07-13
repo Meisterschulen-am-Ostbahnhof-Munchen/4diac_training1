0200157336919f4a9e665d0ccb4b7a01a4119173
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2024-07-13 18:38:56 +0200

    Update Python environment path and file paths in gcf.bat script
    The commit updates the Python environment path to version 5.2 and adjusts file paths for GcfScript.py in the gcf.bat script.

--- Ventilsteuerung/scripts/gcf.bat
+++ Ventilsteuerung/scripts/gcf.bat
@@ -1,9 +1,11 @@
+::Script
+
+@echo off & setlocal
 
 
-
-
-set PATH="C:\Espressif\python_env\idf5.1_py3.11_env\Scripts"
+set PATH="C:\Espressif\python_env\idf5.2_py3.11_env\Scripts"
 
 del ..\4diacIDE-workspace\test\FBs\Ventilsteuerung\DefaultPool.globalconsts
 
-python GcfScript.py --oldfile 4diacIDE-workspace\4diac_training1_empty-main_Uebung2\Ventilsteuerung\ISO-DesignerProjects\Workspace\DefaultPool\Output --newfile 4diacIDE-workspace\4diac_training1_empty-main_Uebung2\Ventilsteuerung\4diacIDE-workspace\test\FBs\Ventilsteuerung
+python GcfScript.py --oldfile ..\ISO-DesignerProjects\Workspace\DefaultPool\Output\DefaultPool.iop.h --newfile ..\4diacIDE-workspace\test\FBs\Ventilsteuerung\DefaultPool
+
