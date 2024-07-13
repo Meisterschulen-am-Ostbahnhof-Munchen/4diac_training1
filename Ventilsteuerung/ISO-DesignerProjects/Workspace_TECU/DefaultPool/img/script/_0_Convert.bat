8575279a0d03ba1ee2828fcb206d017cce352cc4
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2024-07-13 13:33:46 +0200

    Update image files in various sizes and formats.

--- Ventilsteuerung/ISO-DesignerProjects/Workspace_TECU/DefaultPool/img/script/_0_Convert.bat
+++ Ventilsteuerung/ISO-DesignerProjects/Workspace_TECU/DefaultPool/img/script/_0_Convert.bat
@@ -11,13 +11,13 @@ cd ..\img_original
 for %%f in (*.png) do (
     magick identify %%f
     echo 1. crop Bitmap by 2px 
-    magick %%f -shave 2x2 -define png:exclude-chunks=date ..\img_cropped\%%f 
+    magick %%f -shave 2x2 -define png:exclude-chunks=date,time ..\img_cropped\%%f 
     magick identify ..\img_cropped\%%f
     echo 2. resize to 64x64
-    magick ..\img_cropped\%%f -resize 64x64 -define png:exclude-chunks=date ..\img_resized\%%f  
+    magick ..\img_cropped\%%f -resize 64x64 -define png:exclude-chunks=date,time ..\img_resized\%%f  
     magick identify ..\img_resized\%%f
     echo 3. make monochrome PNG
-    magick ..\img_resized\%%f -monochrome -define png:exclude-chunks=date ..\img_monochrome\%%f 
+    magick ..\img_resized\%%f -monochrome -define png:exclude-chunks=date,time ..\img_monochrome\%%f 
     magick identify ..\img_monochrome\%%f
     echo 4. make BMP
     magick ..\img_monochrome\%%f ..\img\%%~nf.bmp
