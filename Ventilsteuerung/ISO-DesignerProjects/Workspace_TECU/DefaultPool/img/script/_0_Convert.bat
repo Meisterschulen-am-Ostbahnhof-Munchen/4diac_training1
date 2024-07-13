956a772b57db1cdcc053aefc46148b5b14cb0962
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2024-07-13 12:51:23 +0200

    Add image processing steps in batch script
    1. Crop images by 2px
    2. Resize to 64x64
    3. Convert to monochrome BMP

--- Ventilsteuerung/ISO-DesignerProjects/Workspace_TECU/DefaultPool/img/script/_0_Convert.bat
+++ Ventilsteuerung/ISO-DesignerProjects/Workspace_TECU/DefaultPool/img/script/_0_Convert.bat
@@ -10,9 +10,15 @@ cd ..\img_original
 
 for %%f in (*.png) do (
     magick identify %%f
-    echo crop Bitmap by 2px 
+    echo 1. crop Bitmap by 2px 
     magick %%f -shave   2x2 ..\img_cropped\%%f
     magick identify ..\img_cropped\%%f
+    echo 2. resize to 64x64
+    magick ..\img_cropped\%%f  -resize  64x64       ..\img_resized\%%f
+    magick identify ..\img_resized\%%f
+    echo 3. make monochrome BMP
+    magick ..\img_resized\%%f  -threshold 85 -type bilevel BMP3:..\img\%%~nf.bmp
+    magick identify ..\img\%%~nf.bmp
 )
 
 
