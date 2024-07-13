ea000a0e62195badbd28736504b24cd719d7a0dd
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2024-07-13 18:49:49 +0200

    Update script paths and file references for Ventilsteuerung. Improve workspace organization and handling of global constants.

--- Ventilsteuerung/scripts/RunSkript.bat
+++ Ventilsteuerung/scripts/RunSkript.bat
@@ -5,7 +5,10 @@
 
 set PATH="C:\Espressif\python_env\idf5.2_py3.11_env\Scripts"
 
-del ..\4diacIDE-workspace\test\FBs\Ventilsteuerung\DefaultPool.globalconsts
+del ..\4diacIDE-workspace\test\FBs\Ventilsteuerung\Workspace\DefaultPool.globalconsts
 
-python GcfScript.py --oldfile ..\ISO-DesignerProjects\Workspace\DefaultPool\Output\DefaultPool.iop.h --newfile ..\4diacIDE-workspace\test\FBs\Ventilsteuerung\DefaultPool
+python GcfScript.py --oldfile ..\ISO-DesignerProjects\Workspace\DefaultPool\Output\DefaultPool.iop.h --newfile ..\4diacIDE-workspace\test\FBs\Ventilsteuerung\Workspace\DefaultPool
 
+del ..\4diacIDE-workspace\test\FBs\Ventilsteuerung\Workspace_TECU\DefaultPool.globalconsts
+
+python GcfScript.py --oldfile ..\ISO-DesignerProjects\Workspace_TECU\DefaultPool\Output\DefaultPool.iop.h --newfile ..\4diacIDE-workspace\test\FBs\Ventilsteuerung\Workspace_TECU\DefaultPool
