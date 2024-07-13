6cbffaed93bcdf7a3643e617b069b13bc76eecd0
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2024-07-13 18:31:29 +0200

    Update XML attributes and add new components to the existing structure. Add objects with specific properties for COutputNumber, COutputText, GraphicObject, CNumberVariable, CFontStyle, and CLineStyle classes in the code.

---
+++ Ventilsteuerung/ISO-DesignerProjects/Workspace_TECU/DefaultPool/Output/DefaultPool.3.c
@@ -0,0 +1,23 @@
+// ISO-Designer ISO 11783   Version 5.6.2.5533 Jetter AG
+// Do not change!
+
+#include "DefaultPool.c.h"
+
+const unsigned long ISO_OP_MEMORY_CLASS isoOP_DefaultPool_Offset_Id[] = {
+	     1,  // WorkingSet_0_Offset
+	    21,  // DataMask_M1_Offset
+	    47,  // Container_Wheel_based_machine_speed_Offset
+	    87,  // SoftKeyMask_S1_Offset
+	    93,  // OutputString_km_h_Offset
+	   114,  // OutputString_m_s_Offset
+	   135,  // OutputNumber_Wheel_based_machine_speed_km_h_Offset
+	   164,  // OutputNumber_Wheel_based_machine_speed_m_s_Offset
+	   193,  // Line_13000_Offset
+	   204,  // Tractor_wheel_slip_Offset
+	   733,  // Battery_disconnect_Offset
+	  1262,  // Work_distance_travelled_Offset
+	  1791,  // NumberVariable_Wheel_based_machine_speed_Offset
+	  1798,  // FontAttributes_23000_Offset
+	  1806,  // FontAttributes_23001_Offset
+	  1814,  // LineAttributes_24000_Offset
+};  // isoOP_DefaultPool_Offset_Id
