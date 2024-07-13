fd0c57bd10de9036440b184e490dcc0b08189478
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2024-07-13 12:37:13 +0200

    Update batch script to remove unnecessary comments and add image cropping functionality. Details: Removed commented lines, added image cropping command for PNG files in the script.

--- Ventilsteuerung/ISO-DesignerProjects/Workspace_TECU/DefaultPool/img/script/_0_Convert.bat
+++ Ventilsteuerung/ISO-DesignerProjects/Workspace_TECU/DefaultPool/img/script/_0_Convert.bat
@@ -1,6 +1,6 @@
 ::Script
 
-::@echo off & setlocal
+@echo off & setlocal
 
 magick -version
 
@@ -9,12 +9,10 @@ cls
 cd ..\img_original
 
 for %%f in (*.png) do (
-    echo %%f
     magick identify %%f
-    ::magick %%f -crop   196x196+2+2 ..\img_cropped\%%f
-    ::magick ..\img_cropped\%%f  -resize  64x64       ..\img_resized\%%f
-    ::magick convert ..\img_resized\%%f  -threshold 85 -type bilevel BMP3:..\img\%%~nf.bmp
+    magick %%f -crop   196x196+2+2 ..\img_cropped\%%f
 )
+
 
 
 pause
