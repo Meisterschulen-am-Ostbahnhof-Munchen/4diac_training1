1ee8fe75160e09139eaa30221932d1d024efb9c7
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2024-07-18 17:00:28 +0200

    Update image processing script to resize and convert images for different purposes. Improve organization and add support for multiple output formats.

--- Ventilsteuerung/ISO-DesignerProjects/Workspace_TECU/DefaultPool/img/script/_0_Convert.bat
+++ Ventilsteuerung/ISO-DesignerProjects/Workspace_TECU/DefaultPool/img/script/_0_Convert.bat
@@ -13,15 +13,22 @@ for %%f in (*.png) do (
     echo 1. crop Bitmap by 2px 
     magick %%f -shave 2x2 -define png:exclude-chunks=date,time ..\img_cropped\%%f 
     magick identify ..\img_cropped\%%f
-    echo 2. resize to 64x64
-    magick ..\img_cropped\%%f -resize 64x64 -define png:exclude-chunks=date,time ..\img_resized\%%f  
-    magick identify ..\img_resized\%%f
-    echo 3. make monochrome PNG
-    magick ..\img_resized\%%f -monochrome -define png:exclude-chunks=date,time ..\img_monochrome\%%f 
-    magick identify ..\img_monochrome\%%f
-    echo 4. make BMP
-    magick ..\img_monochrome\%%f BMP3:..\img\%%~nf.bmp
-    magick identify ..\img\%%~nf.bmp
+    echo 2. resize to 64x64 for SKM
+    magick ..\img_cropped\%%f -resize 64x64 -define png:exclude-chunks=date,time ..\img_resized_SKM\%%f  
+    magick identify ..\img_resized_SKM\%%f
+    echo 2. do not resize for DM
+    echo 3. make monochrome PNG for SKM
+    magick ..\img_resized_SKM\%%f -monochrome -define png:exclude-chunks=date,time ..\img_monochrome_SKM\%%f 
+    magick identify ..\img_monochrome_SKM\%%f
+    echo 3. make monochrome PNG for DM
+    magick ..\img_cropped\%%f -monochrome -define png:exclude-chunks=date,time ..\img_monochrome_DM\%%f 
+    magick identify ..\img_monochrome_DM\%%f
+    echo 4. make BMP for SKM
+    magick ..\img_monochrome_SKM\%%f BMP3:..\img_SKM\%%~nf.bmp
+    magick identify ..\img_SKM\%%~nf.bmp
+    echo 4. make BMP for DM
+    magick ..\img_monochrome_DM\%%f BMP3:..\img_DM\%%~nf.bmp
+    magick identify ..\img_DM\%%~nf.bmp
     echo DONE *************************************
     echo *
     echo *
