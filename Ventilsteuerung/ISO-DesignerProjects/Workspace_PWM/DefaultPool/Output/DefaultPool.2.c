bd3912b124101cb96664153b847ecad001d507f1
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2024-07-31 13:21:03 +0200

    Update UI components properties and add new objects to the project. Add font styles for text elements.

---
+++ Ventilsteuerung/ISO-DesignerProjects/Workspace_PWM/DefaultPool/Output/DefaultPool.2.c
@@ -0,0 +1,23 @@
+// ISO-Designer ISO 11783   Version 5.6.2.5533 Jetter AG
+// Do not change!
+
+#include "DefaultPool.c.h"
+
+const unsigned long ISO_OP_MEMORY_CLASS isoOP_DefaultPool_Offset[] = {
+	     1,  // WorkingSet_0_Offset
+	    21,  // Macro_100_perc_Offset
+	    26,  // DataMask_1000_Offset
+	    76,  // Button_100_perc_Offset
+	   103,  // InputNumber_PWM_DUTY_Offset
+	   141,  // InputNumber_PWM_Value_Offset
+	   179,  // OutputString_percent_Offset
+	   200,  // OutputString_WS_Offset
+	   221,  // OutputNumber_100_Offset
+	   250,  // OutputNumber_8129_Offset
+	   279,  // NumberVariable_PWM_DUTY_Offset
+	   286,  // NumberVariable_PWM_Value_Offset
+	   293,  // FontAttributes_12x16_Offset
+	   301,  // FontAttributes_12x16_B_Offset
+	   309,  // FontAttributes_16x16_Offset
+	   317,  // FontAttributes_WS_Offset
+};  // isoOP_DefaultPool_Offset
---
+++ Ventilsteuerung/ISO-DesignerProjects/Workspace_PWM/DefaultPool/Output/DefaultPool.iop.h
@@ -0,0 +1,24 @@
+// ISO-Designer ISO 11783   Version 5.6.2.5533 Jetter AG
+// Do not change!
+#define ISO_DESIGNATOR_WIDTH                160
+#define ISO_DESIGNATOR_HEIGHT                80
+#define ISO_MASK_SIZE                       480
+#define ISO_VERSION_LABEL             "       "
+#define MASK_WIDTH                          480
+#define MASK_HEIGHT                         480
+#define WorkingSet_0                          0
+#define Macro_100_perc                        1
+#define DataMask_1000                      1000
+#define Button_100_perc                    6006
+#define InputNumber_PWM_DUTY               9000
+#define InputNumber_PWM_Value              9001
+#define OutputString_percent              11005
+#define OutputString_WS                   11500
+#define OutputNumber_100                  12000
+#define OutputNumber_8129                 12001
+#define NumberVariable_PWM_DUTY           21000
+#define NumberVariable_PWM_Value          21001
+#define FontAttributes_12x16              23000
+#define FontAttributes_12x16_B            23001
+#define FontAttributes_16x16              23004
+#define FontAttributes_WS                 23500
