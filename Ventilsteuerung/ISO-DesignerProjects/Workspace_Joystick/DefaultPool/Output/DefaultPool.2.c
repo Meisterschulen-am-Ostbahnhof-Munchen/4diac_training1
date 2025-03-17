db641da859dfd0cdadf583f5735d9e322c2b7fa4
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2025-03-17 15:19:54 +0100

    Update project files and add new output code
    - Changed some property values in configuration files.
    - Added new C source files for default pool offsets.
    - Created header files with definitions for various constants.
    - Updated XML version in working set file.
    - Introduced a workspace file for the joystick project.

---
+++ Ventilsteuerung/ISO-DesignerProjects/Workspace_Joystick/DefaultPool/Output/DefaultPool.2.c
@@ -0,0 +1,11 @@
+// ISO-Designer ISO 11783   Version 5.7.0.6283 Bucher Automation AG
+// Do not change!
+
+#include "DefaultPool.c.h"
+
+const unsigned long ISO_OP_MEMORY_CLASS isoOP_DefaultPool_Offset[] = {
+	     1,  // WorkingSet_0_Offset
+	    21,  // DataMask_1000_Offset
+	    29,  // OutputString_WS_Offset
+	    49,  // FontAttributes_WS_Offset
+};  // isoOP_DefaultPool_Offset
---
+++ Ventilsteuerung/ISO-DesignerProjects/Workspace_Joystick/DefaultPool/Output/DefaultPool.c
@@ -0,0 +1,28 @@
+// ISO-Designer ISO 11783   Version 5.7.0.6283 Bucher Automation AG
+// Do not change!
+
+#include "DefaultPool.iop.h"
+#include "DefaultPool.c.h"
+
+#define WORD(w)  (unsigned char)w, (unsigned char)(w >> 8)
+#define LONG(l)  (unsigned char)l, \
+					(unsigned char)((unsigned long)l >> 8), \
+					(unsigned char)((unsigned long)l >> 16), \
+					(unsigned char)((unsigned long)l >> 24)
+#define ID(id)           WORD(id)
+#define REF(id)          WORD(id)
+#define XYREF(id, x, y)  WORD(id), WORD(x), WORD(y)
+#define MACRO(ev, id)    ev, id
+#define COLOR(c)         c
+
+const unsigned char ISO_OP_MEMORY_CLASS isoOP_DefaultPool[] = {
+	17,
+	ID(WorkingSet_0), TYPEID_WORKSET, COLOR_SILVER, 1, ID(DataMask_1000), 1, 0, 2,
+		XYREF(OutputString_WS, 0, 0),
+		'e', 'n',
+		'd', 'e',
+	ID(DataMask_1000), TYPEID_DATAMASK, COLOR_SILVER, ID(ID_NULL), 0, 0, 
+	ID(OutputString_WS), TYPEID_OUTSTR, WORD(72), WORD(72), COLOR_WHITE, ID(FontAttributes_WS), 1,
+		ID(ID_NULL), 0, WORD(3), 'J', 'o', 'y', 0,
+	ID(FontAttributes_WS), TYPEID_FONTATTR, COLOR_BLACK, 3, 0, 0, 0, 
+}; // isoOP_DefaultPool
