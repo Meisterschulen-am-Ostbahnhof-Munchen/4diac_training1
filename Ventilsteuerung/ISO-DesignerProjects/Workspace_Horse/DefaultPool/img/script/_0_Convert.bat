6ffc5b0beb32584eaa427f2aed22cbe205c40dff
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2026-04-22 14:36:32 +0200

    Refactor: Improve image conversion script reliability
    This change enhances the robustness of the image conversion batch script by enabling delayed expansion for correct variable handling within loops and adding quotes to paths to support filenames with spaces. Directory creation is also updated to prevent errors if target folders already exist.

--- Ventilsteuerung/ISO-DesignerProjects/Workspace_Horse/DefaultPool/img/script/_0_Convert.bat
+++ Ventilsteuerung/ISO-DesignerProjects/Workspace_Horse/DefaultPool/img/script/_0_Convert.bat
@@ -1,6 +1,6 @@
 ::Script
 
-@echo off & setlocal
+@echo off & setlocal EnableDelayedExpansion
 
 magick -version
 
@@ -9,82 +9,93 @@ cls
 cd ..\img_original
 
 for %%f in (*.gif) do (
-    magick identify %%f
+    magick identify "%%f"
     echo.
-    echo Processing %%f ...
+    echo Processing "%%f" ...
+    
+    set "fname=%%~nf"
+    
     echo 1. Extract frames from GIF
-    mkdir ..\img_frames\%%~nf 2>nul
-    magick %%f -coalesce -define png:exclude-chunks=date,time ..\img_frames\%%~nf\frame_%%02d.png
-    echo    Frames extracted to ..\img_frames\%%~nf\
+    if not exist "..\img_frames\!fname!" mkdir "..\img_frames\!fname!"
+    magick "%%f" -coalesce -define png:exclude-chunks=date,time "..\img_frames\!fname!\frame_%%02d.png"
+    echo    Frames extracted to ..\img_frames\!fname!\
     echo.
+    
     echo 2. resize frames to 64x64 for SKM
-    mkdir ..\img_resized_SKM\%%~nf 2>nul
-    for %%g in (..\img_frames\%%~nf\*.png) do (
-        magick %%g -resize 64x64 -define png:exclude-chunks=date,time ..\img_resized_SKM\%%~nf\%%~nxg
+    if not exist "..\img_resized_SKM\!fname!" mkdir "..\img_resized_SKM\!fname!"
+    for %%g in (..\img_frames\!fname!\*.png) do (
+        magick "%%g" -resize 64x64 -define png:exclude-chunks=date,time "..\img_resized_SKM\!fname!\%%~nxg"
     )
-    echo    Resized frames in ..\img_resized_SKM\%%~nf\
+    echo    Resized frames in ..\img_resized_SKM\!fname!\
     echo.
+
     echo 3. make monochrome PNG for SKM
-    mkdir ..\img_monochrome_SKM\%%~nf 2>nul
-    for %%g in (..\img_resized_SKM\%%~nf\*.png) do (
-        magick %%g -monochrome -define png:exclude-chunks=date,time ..\img_monochrome_SKM\%%~nf\%%~nxg
+    if not exist "..\img_monochrome_SKM\!fname!" mkdir "..\img_monochrome_SKM\!fname!"
+    for %%g in (..\img_resized_SKM\!fname!\*.png) do (
+        magick "%%g" -monochrome -define png:exclude-chunks=date,time "..\img_monochrome_SKM\!fname!\%%~nxg"
     )
-    echo    Monochrome frames in ..\img_monochrome_SKM\%%~nf\
+    echo    Monochrome frames in ..\img_monochrome_SKM\!fname!\
     echo.
+
     echo 4. make BMP for SKM
-    mkdir ..\img_SKM\%%~nf 2>nul
-    for %%g in (..\img_monochrome_SKM\%%~nf\*.png) do (
-        magick %%g BMP3:..\img_SKM\%%~nf\%%~ng.bmp
+    if not exist "..\img_SKM\!fname!" mkdir "..\img_SKM\!fname!"
+    for %%g in (..\img_monochrome_SKM\!fname!\*.png) do (
+        magick "%%g" BMP3:"..\img_SKM\!fname!\%%~ng.bmp"
     )
-    echo    BMP frames in ..\img_SKM\%%~nf\
+    echo    BMP frames in ..\img_SKM\!fname!\
     echo.
+
     echo 5. make 256 color PNG for SKM
-    mkdir ..\img_256_SKM\%%~nf 2>nul
-    for %%g in (..\img_resized_SKM\%%~nf\*.png) do (
-        magick %%g -colors 256 -dither None -define png:exclude-chunks=date,time ..\img_256_SKM\%%~nf\%%~nxg
+    if not exist "..\img_256_SKM\!fname!" mkdir "..\img_256_SKM\!fname!"
+    for %%g in (..\img_resized_SKM\!fname!\*.png) do (
+        magick "%%g" -colors 256 -dither None -define png:exclude-chunks=date,time "..\img_256_SKM\!fname!\%%~nxg"
     )
-    echo    256 color frames in ..\img_256_SKM\%%~nf\
+    echo    256 color frames in ..\img_256_SKM\!fname!\
     echo.
+
     echo 6. make BMP for SKM 256 colors
-    mkdir ..\img_SKM_256\%%~nf 2>nul
-    for %%g in (..\img_256_SKM\%%~nf\*.png) do (
-        magick %%g -type Palette -compress none BMP3:..\img_SKM_256\%%~nf\%%~ng.bmp
+    if not exist "..\img_SKM_256\!fname!" mkdir "..\img_SKM_256\!fname!"
+    for %%g in (..\img_256_SKM\!fname!\*.png) do (
+        magick "%%g" -type Palette -compress none BMP3:"..\img_SKM_256\!fname!\%%~ng.bmp"
     )
-    echo    BMP 256 frames in ..\img_SKM_256\%%~nf\
+    echo    BMP 256 frames in ..\img_SKM_256\!fname!\
     echo.
+
     echo 7. make monochrome PNG for DM (full size)
-    mkdir ..\img_monochrome_DM\%%~nf 2>nul
-    for %%g in (..\img_frames\%%~nf\*.png) do (
-        magick %%g -monochrome -define png:exclude-chunks=date,time ..\img_monochrome_DM\%%~nf\%%~nxg
+    if not exist "..\img_monochrome_DM\!fname!" mkdir "..\img_monochrome_DM\!fname!"
+    for %%g in (..\img_frames\!fname!\*.png) do (
+        magick "%%g" -monochrome -define png:exclude-chunks=date,time "..\img_monochrome_DM\!fname!\%%~nxg"
     )
-    echo    Monochrome DM frames in ..\img_monochrome_DM\%%~nf\
+    echo    Monochrome DM frames in ..\img_monochrome_DM\!fname!\
     echo.
+
     echo 8. make BMP for DM
-    mkdir ..\img_DM\%%~nf 2>nul
-    for %%g in (..\img_monochrome_DM\%%~nf\*.png) do (
-        magick %%g BMP3:..\img_DM\%%~nf\%%~ng.bmp
+    if not exist "..\img_DM\!fname!" mkdir "..\img_DM\!fname!"
+    for %%g in (..\img_monochrome_DM\!fname!\*.png) do (
+        magick "%%g" BMP3:"..\img_DM\!fname!\%%~ng.bmp"
     )
-    echo    BMP DM frames in ..\img_DM\%%~nf\
+    echo    BMP DM frames in ..\img_DM\!fname!\
     echo.
+
     echo 9. make 256 color PNG for DM (full size)
-    mkdir ..\img_256_DM\%%~nf 2>nul
-    for %%g in (..\img_frames\%%~nf\*.png) do (
-        magick %%g -colors 256 -dither None -define png:exclude-chunks=date,time ..\img_256_DM\%%~nf\%%~nxg
+    if not exist "..\img_256_DM\!fname!" mkdir "..\img_256_DM\!fname!"
+    for %%g in (..\img_frames\!fname!\*.png) do (
+        magick "%%g" -colors 256 -dither None -define png:exclude-chunks=date,time "..\img_256_DM\!fname!\%%~nxg"
     )
-    echo    256 color DM frames in ..\img_256_DM\%%~nf\
+    echo    256 color DM frames in ..\img_256_DM\!fname!\
     echo.
+
     echo 10. make BMP for DM 256 colors
-    mkdir ..\img_DM_256\%%~nf 2>nul
-    for %%g in (..\img_256_DM\%%~nf\*.png) do (
-        magick %%g -type Palette -compress none BMP3:..\img_DM_256\%%~nf\%%~ng.bmp
+    if not exist "..\img_DM_256\!fname!" mkdir "..\img_DM_256\!fname!"
+    for %%g in (..\img_256_DM\!fname!\*.png) do (
+        magick "%%g" -type Palette -compress none BMP3:"..\img_DM_256\!fname!\%%~ng.bmp"
     )
-    echo    BMP 256 DM frames in ..\img_DM_256\%%~nf\
+    echo    BMP 256 DM frames in ..\img_DM_256\!fname!\
     echo.
+    
     echo DONE *************************************
     echo *
     echo *
 )
-
-
 
 pause
