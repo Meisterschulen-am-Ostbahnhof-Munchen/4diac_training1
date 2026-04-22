22eb831f4082d0ec31ae1faf71f43d91c181ebbc
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2026-04-22 13:49:10 +0200

    Refactor: Remove image cropping from conversion script
    The 2-pixel image cropping step (`-shave 2x2`) has been removed from the image conversion workflow. This streamlines the process by directly using the extracted frames for subsequent resizing and monochrome conversion, eliminating an unnecessary modification.

--- Ventilsteuerung/ISO-DesignerProjects/Workspace_Horse/DefaultPool/img/script/_0_Convert.bat
+++ Ventilsteuerung/ISO-DesignerProjects/Workspace_Horse/DefaultPool/img/script/_0_Convert.bat
@@ -17,42 +17,35 @@ for %%f in (*.gif) do (
     magick %%f -coalesce -define png:exclude-chunks=date,time ..\img_frames\%%~nf\frame_%%02d.png
     echo    Frames extracted to ..\img_frames\%%~nf\
     echo.
-    echo 2. crop frames by 2px
-    mkdir ..\img_cropped\%%~nf 2>nul
-    for %%g in (..\img_frames\%%~nf\*.png) do (
-        magick %%g -shave 2x2 -define png:exclude-chunks=date,time ..\img_cropped\%%~nf\%%~nxg
-    )
-    echo    Cropped frames in ..\img_cropped\%%~nf\
-    echo.
-    echo 3. resize cropped frames to 64x64 for SKM
+    echo 2. resize frames to 64x64 for SKM
     mkdir ..\img_resized_SKM\%%~nf 2>nul
-    for %%g in (..\img_cropped\%%~nf\*.png) do (
+    for %%g in (..\img_frames\%%~nf\*.png) do (
         magick %%g -resize 64x64 -define png:exclude-chunks=date,time ..\img_resized_SKM\%%~nf\%%~nxg
     )
     echo    Resized frames in ..\img_resized_SKM\%%~nf\
     echo.
-    echo 4. make monochrome PNG for SKM
+    echo 3. make monochrome PNG for SKM
     mkdir ..\img_monochrome_SKM\%%~nf 2>nul
     for %%g in (..\img_resized_SKM\%%~nf\*.png) do (
         magick %%g -monochrome -define png:exclude-chunks=date,time ..\img_monochrome_SKM\%%~nf\%%~nxg
     )
     echo    Monochrome frames in ..\img_monochrome_SKM\%%~nf\
     echo.
-    echo 5. make BMP for SKM
+    echo 4. make BMP for SKM
     mkdir ..\img_SKM\%%~nf 2>nul
     for %%g in (..\img_monochrome_SKM\%%~nf\*.png) do (
         magick %%g BMP3:..\img_SKM\%%~nf\%%~ng.bmp
     )
     echo    BMP frames in ..\img_SKM\%%~nf\
     echo.
-    echo 6. make monochrome PNG for DM (full size)
+    echo 5. make monochrome PNG for DM (full size)
     mkdir ..\img_monochrome_DM\%%~nf 2>nul
-    for %%g in (..\img_cropped\%%~nf\*.png) do (
+    for %%g in (..\img_frames\%%~nf\*.png) do (
         magick %%g -monochrome -define png:exclude-chunks=date,time ..\img_monochrome_DM\%%~nf\%%~nxg
     )
     echo    Monochrome DM frames in ..\img_monochrome_DM\%%~nf\
     echo.
-    echo 7. make BMP for DM
+    echo 6. make BMP for DM
     mkdir ..\img_DM\%%~nf 2>nul
     for %%g in (..\img_monochrome_DM\%%~nf\*.png) do (
         magick %%g BMP3:..\img_DM\%%~nf\%%~ng.bmp
