fbdac1064263fc30c0c9f929f738c98d9e777a59
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2026-04-22 13:49:49 +0200

    Feat: Implement 256-color image generation for SKM and DM
    This expands the image conversion workflow to produce 256-color PNG and BMP assets, offering more options for display on SKM and DM HMI systems.

--- Ventilsteuerung/ISO-DesignerProjects/Workspace_Horse/DefaultPool/img/script/_0_Convert.bat
+++ Ventilsteuerung/ISO-DesignerProjects/Workspace_Horse/DefaultPool/img/script/_0_Convert.bat
@@ -38,19 +38,47 @@ for %%f in (*.gif) do (
     )
     echo    BMP frames in ..\img_SKM\%%~nf\
     echo.
-    echo 5. make monochrome PNG for DM (full size)
+    echo 5. make 256 color PNG for SKM
+    mkdir ..\img_256_SKM\%%~nf 2>nul
+    for %%g in (..\img_resized_SKM\%%~nf\*.png) do (
+        magick %%g -colors 256 -dither None -define png:exclude-chunks=date,time ..\img_256_SKM\%%~nf\%%~nxg
+    )
+    echo    256 color frames in ..\img_256_SKM\%%~nf\
+    echo.
+    echo 6. make BMP for SKM 256 colors
+    mkdir ..\img_SKM_256\%%~nf 2>nul
+    for %%g in (..\img_256_SKM\%%~nf\*.png) do (
+        magick %%g -type Palette -compress none BMP3:..\img_SKM_256\%%~nf\%%~ng.bmp
+    )
+    echo    BMP 256 frames in ..\img_SKM_256\%%~nf\
+    echo.
+    echo 7. make monochrome PNG for DM (full size)
     mkdir ..\img_monochrome_DM\%%~nf 2>nul
     for %%g in (..\img_frames\%%~nf\*.png) do (
         magick %%g -monochrome -define png:exclude-chunks=date,time ..\img_monochrome_DM\%%~nf\%%~nxg
     )
     echo    Monochrome DM frames in ..\img_monochrome_DM\%%~nf\
     echo.
-    echo 6. make BMP for DM
+    echo 8. make BMP for DM
     mkdir ..\img_DM\%%~nf 2>nul
     for %%g in (..\img_monochrome_DM\%%~nf\*.png) do (
         magick %%g BMP3:..\img_DM\%%~nf\%%~ng.bmp
     )
     echo    BMP DM frames in ..\img_DM\%%~nf\
+    echo.
+    echo 9. make 256 color PNG for DM (full size)
+    mkdir ..\img_256_DM\%%~nf 2>nul
+    for %%g in (..\img_frames\%%~nf\*.png) do (
+        magick %%g -colors 256 -dither None -define png:exclude-chunks=date,time ..\img_256_DM\%%~nf\%%~nxg
+    )
+    echo    256 color DM frames in ..\img_256_DM\%%~nf\
+    echo.
+    echo 10. make BMP for DM 256 colors
+    mkdir ..\img_DM_256\%%~nf 2>nul
+    for %%g in (..\img_256_DM\%%~nf\*.png) do (
+        magick %%g -type Palette -compress none BMP3:..\img_DM_256\%%~nf\%%~ng.bmp
+    )
+    echo    BMP 256 DM frames in ..\img_DM_256\%%~nf\
     echo.
     echo DONE *************************************
     echo *
