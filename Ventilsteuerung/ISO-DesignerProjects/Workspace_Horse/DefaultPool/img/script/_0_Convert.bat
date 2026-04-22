c2a492ac867347f2825b2b7ee3d621ba9426fa04
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2026-04-22 16:04:03 +0200

    Refactor _0_Convert.bat for streamlined DM 256-color HMI image generation
    Simplifies the image conversion pipeline by removing output targets for SKM and other unused DM image formats. The script now exclusively processes GIFs for `img_DM_256`, converting frames to 8-bit BMPs with a web-safe palette. It also refines the median-based background extraction and replacement with pink (#FF00FF) to further optimize for RLE compression.

--- Ventilsteuerung/ISO-DesignerProjects/Workspace_Horse/DefaultPool/img/script/_0_Convert.bat
+++ Ventilsteuerung/ISO-DesignerProjects/Workspace_Horse/DefaultPool/img/script/_0_Convert.bat
@@ -1,9 +1,6 @@
-::Script
-
 @echo off & setlocal EnableDelayedExpansion
 
 magick -version
-
 
 cls
 cd ..\img_original
@@ -21,80 +18,28 @@ for %%f in (*.gif) do (
     echo    Frames extracted to ..\img_frames\!fname!\
     echo.
     
-    echo 2. resize frames to 64x64 for SKM
-    if not exist "..\img_resized_SKM\!fname!" mkdir "..\img_resized_SKM\!fname!"
-    for %%g in (..\img_frames\!fname!\*.png) do (
-        magick "%%g" -resize 64x64 -define png:exclude-chunks=date,time "..\img_resized_SKM\!fname!\%%~nxg"
-    )
-    echo    Resized frames in ..\img_resized_SKM\!fname!\
-    echo.
-
-    echo 3. make monochrome PNG for SKM
-    if not exist "..\img_monochrome_SKM\!fname!" mkdir "..\img_monochrome_SKM\!fname!"
-    for %%g in (..\img_resized_SKM\!fname!\*.png) do (
-        magick "%%g" -monochrome -define png:exclude-chunks=date,time "..\img_monochrome_SKM\!fname!\%%~nxg"
-    )
-    echo    Monochrome frames in ..\img_monochrome_SKM\!fname!\
-    echo.
-
-    echo 4. make BMP for SKM
-    if not exist "..\img_SKM\!fname!" mkdir "..\img_SKM\!fname!"
-    for %%g in (..\img_monochrome_SKM\!fname!\*.png) do (
-        magick "%%g" BMP3:"..\img_SKM\!fname!\%%~ng.bmp"
-    )
-    echo    BMP frames in ..\img_SKM\!fname!\
-    echo.
-
-    echo 5. calculate common background for SKM and create 256 color PNGs (pink = transparent / background)
-    if not exist "..\img_256_SKM\!fname!" mkdir "..\img_256_SKM\!fname!"
-    magick ..\img_resized_SKM\!fname!\frame_*.png -evaluate-sequence Median "..\img_resized_SKM\!fname!\background.png"
-    for %%g in (..\img_resized_SKM\!fname!\frame_*.png) do (
-        magick "..\img_resized_SKM\!fname!\background.png" "%%g" -fuzz 2%% -compose ChangeMask -composite -background "#FF00FF" -alpha background -alpha remove -colors 256 -dither None -define png:exclude-chunks=date,time "..\img_256_SKM\!fname!\%%~nxg"
-    )
-    echo    256 color frames (background removed) in ..\img_256_SKM\!fname!\
-    echo.
-
-    echo 6. make BMP for SKM 256 colors (including frame_BG.bmp)
-    if not exist "..\img_SKM_256\!fname!" mkdir "..\img_SKM_256\!fname!"
-    for %%g in (..\img_256_SKM\!fname!\*.png) do (
-        magick "%%g" -type Palette -compress none BMP3:"..\img_SKM_256\!fname!\%%~ng.bmp"
-    )
-    magick "..\img_resized_SKM\!fname!\background.png" -type Palette -compress none BMP3:"..\img_SKM_256\!fname!\frame_BG.bmp"
-    echo    BMP 256 frames + frame_BG.bmp in ..\img_SKM_256\!fname!\
-    echo.
-
-    echo 7. make monochrome PNG for DM (full size)
-    if not exist "..\img_monochrome_DM\!fname!" mkdir "..\img_monochrome_DM\!fname!"
-    for %%g in (..\img_frames\!fname!\*.png) do (
-        magick "%%g" -monochrome -define png:exclude-chunks=date,time "..\img_monochrome_DM\!fname!\%%~nxg"
-    )
-    echo    Monochrome DM frames in ..\img_monochrome_DM\!fname!\
-    echo.
-
-    echo 8. make BMP for DM
-    if not exist "..\img_DM\!fname!" mkdir "..\img_DM\!fname!"
-    for %%g in (..\img_monochrome_DM\!fname!\*.png) do (
-        magick "%%g" BMP3:"..\img_DM\!fname!\%%~ng.bmp"
-    )
-    echo    BMP DM frames in ..\img_DM\!fname!\
-    echo.
-
-    echo 9. calculate common background for DM and create 256 color PNGs (pink = transparent / background)
-    if not exist "..\img_256_DM\!fname!" mkdir "..\img_256_DM\!fname!"
-    magick ..\img_frames\!fname!\frame_*.png -evaluate-sequence Median "..\img_frames\!fname!\background.png"
-    for %%g in (..\img_frames\!fname!\frame_*.png) do (
-        magick "..\img_frames\!fname!\background.png" "%%g" -fuzz 2%% -compose ChangeMask -composite -background "#FF00FF" -alpha background -alpha remove -colors 256 -dither None -define png:exclude-chunks=date,time "..\img_256_DM\!fname!\%%~nxg"
-    )
-    echo    256 color DM frames (background removed) in ..\img_256_DM\!fname!\
-    echo.
-
-    echo 10. make BMP for DM 256 colors (including frame_BG.bmp)
+    echo 2. Convert frames to 8-bit BMP with Web-Safe palette (216 colors)
     if not exist "..\img_DM_256\!fname!" mkdir "..\img_DM_256\!fname!"
-    for %%g in (..\img_256_DM\!fname!\*.png) do (
-        magick "%%g" -type Palette -compress none BMP3:"..\img_DM_256\!fname!\%%~ng.bmp"
+    for %%g in (..\img_frames\!fname!\*.png) do (
+        magick "%%g" -remap netscape: -type Palette BMP3:"..\img_DM_256\!fname!\%%~ng.bmp"
     )
-    magick "..\img_frames\!fname!\background.png" -type Palette -compress none BMP3:"..\img_DM_256\!fname!\frame_BG.bmp"
-    echo    BMP 256 DM frames + frame_BG.bmp in ..\img_DM_256\!fname!\
+    echo    Web-Safe BMP frames in ..\img_DM_256\!fname!\
+    echo.
+
+    echo 3. Calculate common background from all frames (Median)
+    magick ..\img_DM_256\!fname!\frame_*.bmp -evaluate-sequence Median -remap netscape: -type Palette BMP3:"..\img_DM_256\!fname!\frame_BG.bmp"
+    echo    frame_BG.bmp created in ..\img_DM_256\!fname!\
+    echo.
+
+    echo 4. Replace background pixels with pink (#FF00FF) for RLE compression
+    for %%g in (..\img_DM_256\!fname!\frame_*.bmp) do (
+        if /I not "%%~nxg"=="frame_BG.bmp" (
+            magick "%%g" "..\img_DM_256\!fname!\frame_BG.bmp" -compose Difference -composite -threshold 0 "..\img_DM_256\!fname!\tmp_mask.png"
+            magick "%%g" ( "..\img_DM_256\!fname!\tmp_mask.png" -alpha off ) -compose CopyOpacity -composite -background "#FF00FF" -alpha background -alpha remove -remap netscape: -type Palette BMP3:"%%g"
+            del "..\img_DM_256\!fname!\tmp_mask.png"
+        )
+    )
+    echo    Background pixels replaced with pink in all frames
     echo.
 
     echo DONE *************************************
