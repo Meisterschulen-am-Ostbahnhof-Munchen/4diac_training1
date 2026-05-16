ea89360f4089acb1647a640f88b1c4dde53df470
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2026-05-16 20:30:13 +0200

    Add input and output components for threshold and hysteresis values
    Enhanced the ISO Designer project by introducing new input and output components for managing threshold and hysteresis parameters. This change incorporates text and numeric input components along with updated font attributes to improve data visibility and user interaction within the existing layout. Additionally, adjustments were made to certain offsets and object pools to accommodate these components.

--- Ventilsteuerung/ISO-DesignerProjects/Workspace_PWM/DefaultPool/Output/DefaultPool.3.c
+++ Ventilsteuerung/ISO-DesignerProjects/Workspace_PWM/DefaultPool/Output/DefaultPool.3.c
@@ -11,28 +11,33 @@ const unsigned long ISO_OP_MEMORY_CLASS isoOP_DefaultPool_Offset_Id[] = {
 	    60,  // Macro_000_perc_Offset
 	    73,  // Macro_025_perc_Offset
 	    86,  // DataMask_Offset
-	   184,  // Button_050_perc_Offset
-	   211,  // Button_075_perc_Offset
-	   238,  // Button_000_perc_Offset
-	   265,  // Button_025_perc_Offset
-	   292,  // Button_100_perc_Offset
-	   319,  // InputNumber_PWM_DUTY_Offset
-	   357,  // InputNumber_PWM_Value_Offset
-	   395,  // InputNumber_PWM_DUTY_OUT_Offset
-	   433,  // OutputString_percent_Offset
-	   452,  // OutputString_WS_Offset
-	   472,  // OutputNumber_100_Offset
-	   501,  // OutputNumber_8129_Offset
-	   530,  // OutputNumber_050_Offset
-	   559,  // OutputNumber_075_Offset
-	   588,  // OutputNumber_000_Offset
-	   617,  // OutputNumber_025_Offset
-	   646,  // Meter_Offset
-	   667,  // LinearBargraph_Offset
-	   691,  // ArchedBargraph_Offset
-	   718,  // NumberVariable_PWM_Value_Offset
-	   725,  // FontAttributes_12x16_Offset
-	   733,  // FontAttributes_12x16_B_Offset
-	   741,  // FontAttributes_16x16_Offset
-	   749,  // FontAttributes_WS_Offset
+	   208,  // Button_050_perc_Offset
+	   235,  // Button_075_perc_Offset
+	   262,  // Button_000_perc_Offset
+	   289,  // Button_025_perc_Offset
+	   316,  // Button_100_perc_Offset
+	   343,  // InputNumber_PWM_DUTY_Offset
+	   381,  // InputNumber_PWM_Value_Offset
+	   419,  // InputNumber_PWM_DUTY_OUT_Offset
+	   457,  // InputNumber_THRESHOLD_Offset
+	   495,  // InputNumber_HYSTERESIS_Offset
+	   533,  // OutputString_THRESHOLD_Offset
+	   559,  // OutputString_HYSTERESIS_Offset
+	   586,  // OutputString_percent_Offset
+	   605,  // OutputString_WS_Offset
+	   625,  // OutputNumber_100_Offset
+	   654,  // OutputNumber_8129_Offset
+	   683,  // OutputNumber_050_Offset
+	   712,  // OutputNumber_075_Offset
+	   741,  // OutputNumber_000_Offset
+	   770,  // OutputNumber_025_Offset
+	   799,  // Meter_Offset
+	   820,  // LinearBargraph_Offset
+	   844,  // ArchedBargraph_Offset
+	   871,  // NumberVariable_PWM_Value_Offset
+	   878,  // FontAttributes_12x16_Offset
+	   886,  // FontAttributes_12x16_B_Offset
+	   894,  // FontAttributes_small_Offset
+	   902,  // FontAttributes_16x16_Offset
+	   910,  // FontAttributes_WS_Offset
 };  // isoOP_DefaultPool_Offset_Id
--- Ventilsteuerung/ISO-DesignerProjects/Workspace_PWM/DefaultPool/Output/DefaultPool.c.h
+++ Ventilsteuerung/ISO-DesignerProjects/Workspace_PWM/DefaultPool/Output/DefaultPool.c.h
@@ -8,37 +8,42 @@
 #define Macro_000_perc_Offset                                  60
 #define Macro_025_perc_Offset                                  73
 #define DataMask_Offset                                        86
-#define Button_050_perc_Offset                                184
-#define Button_075_perc_Offset                                211
-#define Button_000_perc_Offset                                238
-#define Button_025_perc_Offset                                265
-#define Button_100_perc_Offset                                292
-#define InputNumber_PWM_DUTY_Offset                           319
-#define InputNumber_PWM_Value_Offset                          357
-#define InputNumber_PWM_DUTY_OUT_Offset                       395
-#define OutputString_percent_Offset                           433
-#define OutputString_WS_Offset                                452
-#define OutputNumber_100_Offset                               472
-#define OutputNumber_8129_Offset                              501
-#define OutputNumber_050_Offset                               530
-#define OutputNumber_075_Offset                               559
-#define OutputNumber_000_Offset                               588
-#define OutputNumber_025_Offset                               617
-#define Meter_Offset                                          646
-#define LinearBargraph_Offset                                 667
-#define ArchedBargraph_Offset                                 691
-#define NumberVariable_PWM_Value_Offset                       718
-#define FontAttributes_12x16_Offset                           725
-#define FontAttributes_12x16_B_Offset                         733
-#define FontAttributes_16x16_Offset                           741
-#define FontAttributes_WS_Offset                              749
+#define Button_050_perc_Offset                                208
+#define Button_075_perc_Offset                                235
+#define Button_000_perc_Offset                                262
+#define Button_025_perc_Offset                                289
+#define Button_100_perc_Offset                                316
+#define InputNumber_PWM_DUTY_Offset                           343
+#define InputNumber_PWM_Value_Offset                          381
+#define InputNumber_PWM_DUTY_OUT_Offset                       419
+#define InputNumber_THRESHOLD_Offset                          457
+#define InputNumber_HYSTERESIS_Offset                         495
+#define OutputString_THRESHOLD_Offset                         533
+#define OutputString_HYSTERESIS_Offset                        559
+#define OutputString_percent_Offset                           586
+#define OutputString_WS_Offset                                605
+#define OutputNumber_100_Offset                               625
+#define OutputNumber_8129_Offset                              654
+#define OutputNumber_050_Offset                               683
+#define OutputNumber_075_Offset                               712
+#define OutputNumber_000_Offset                               741
+#define OutputNumber_025_Offset                               770
+#define Meter_Offset                                          799
+#define LinearBargraph_Offset                                 820
+#define ArchedBargraph_Offset                                 844
+#define NumberVariable_PWM_Value_Offset                       871
+#define FontAttributes_12x16_Offset                           878
+#define FontAttributes_12x16_B_Offset                         886
+#define FontAttributes_small_Offset                           894
+#define FontAttributes_16x16_Offset                           902
+#define FontAttributes_WS_Offset                              910
 
 #define ISO_OP_MEMORY_CLASS
 
-#define ISO_OP_DefaultPool_Size    757
+#define ISO_OP_DefaultPool_Size    918
 extern const unsigned char ISO_OP_MEMORY_CLASS isoOP_DefaultPool[];
 
-#define ISO_OP_DefaultPool_ObjectNumber     31
+#define ISO_OP_DefaultPool_ObjectNumber     36
 extern const unsigned long ISO_OP_MEMORY_CLASS isoOP_DefaultPool_Offset[];
 extern const unsigned long ISO_OP_MEMORY_CLASS isoOP_DefaultPool_Offset_Id[];
 #define ISO_OP_DefaultPool_Scale_Offset      1
