3c0da021005ef1236886144c686ebb3e3be4ec66
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2026-07-20 04:49:44 +0200

    feat: add functionality for testing ISO projects
    Introduce new files in the `Workspace_Tester` directory and modify the `.gitignore` to exclude `.zip` files. This update supports ISO-DesignerProjects by adding various XML, image, and output files, facilitating testing and development within the Ventilsteuerung project.

---
+++ Ventilsteuerung/ISO-DesignerProjects/Workspace_Tester/DefaultPool/Output/DefaultPool.3.c
@@ -0,0 +1,56 @@
+// ISO-Designer ISO 11783   Version 5.7.2.6664 Bucher Automation AG
+// Do not change!
+
+#include "DefaultPool.c.h"
+
+const unsigned long ISO_OP_MEMORY_CLASS isoOP_DefaultPool_Offset_Id[] = {
+	     1,  // WorkingSet_Offset
+	    21,  // DataMask_M1_Offset
+	    41,  // Container_Q_Offset
+	   123,  // Container_I_Offset
+	   181,  // Button_Q01_Offset
+	   200,  // Button_Q02_Offset
+	   219,  // Button_Q03_Offset
+	   238,  // Button_Q04_Offset
+	   257,  // Button_Q05_Offset
+	   276,  // Button_Q06_Offset
+	   295,  // Button_Q07_Offset
+	   314,  // Button_Q08_Offset
+	   333,  // Button_Q09_Offset
+	   352,  // Button_Q10_Offset
+	   371,  // Button_Q11_Offset
+	   390,  // Button_Q12_Offset
+	   409,  // Button_I01_Offset
+	   428,  // Button_I02_Offset
+	   447,  // Button_I03_Offset
+	   466,  // Button_I04_Offset
+	   485,  // Button_I05_Offset
+	   504,  // Button_I06_Offset
+	   523,  // Button_I07_Offset
+	   542,  // Button_I08_Offset
+	   561,  // OutputString_WorkingSet_0_Offset
+	   590,  // OutputNumber_12000_Offset
+	   619,  // OutputNumber_12001_Offset
+	   648,  // OutputNumber_12002_Offset
+	   677,  // OutputNumber_12003_Offset
+	   706,  // OutputNumber_12004_Offset
+	   735,  // OutputNumber_12005_Offset
+	   764,  // OutputNumber_12006_Offset
+	   793,  // OutputNumber_12007_Offset
+	   822,  // OutputNumber_12008_Offset
+	   851,  // OutputNumber_12009_Offset
+	   880,  // OutputNumber_12010_Offset
+	   909,  // OutputNumber_12011_Offset
+	   938,  // OutputNumber_12012_Offset
+	   967,  // OutputNumber_12013_Offset
+	   996,  // OutputNumber_12014_Offset
+	  1025,  // OutputNumber_12015_Offset
+	  1054,  // OutputNumber_12016_Offset
+	  1083,  // OutputNumber_12017_Offset
+	  1112,  // OutputNumber_12018_Offset
+	  1141,  // OutputNumber_12019_Offset
+	  1170,  // Logos_icon_Offset
+	  1699,  // FontAttributes_6x8_Offset
+	  1707,  // FontAttributes_24x32_Offset
+	  1715,  // AuxFunction2_X1_Offset
+};  // isoOP_DefaultPool_Offset_Id
